3ba0d2f6b88dfb0054529b44540522f58cb167ed
Laura Mendez <lauramendez@Lauras-Air.lan>
2023-04-30 16:28:14 -0700

    first input'

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+class  {}
+
+//array of questions for user input
+const question = [
+    {
+        type: "input",
+        name: "text",
+        message: "Please enter text for logo. (Text should not be more than 3 characters)",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "Please enter a text color keyword (or a hexadecimal number):",
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "Please enter shape color keyword (or a hexadecimal number):",
+    },
+    {
+        type: "list",
+        name: "shape-image",
+        message: "Please choose which shape you would like?",
+        choice: ["Circle","Square","Triangle"],
+    },
+];
+
+
+
+module.export = ;
